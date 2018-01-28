6922662f95f98c8434992b70ecab34d3cf34674b
lulu.li29 <s987337613@gmail.com>
2018-01-28 20:05:13 +0800

    add test Black and White

--- Simple-Fun/Simple-Fun/UnitTest1.cs
+++ Simple-Fun/Simple-Fun/UnitTest1.cs
@@ -1,14 +1,23 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+﻿using NUnit.Framework;
 namespace Simple_Fun
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
+        [Test]
         public void TestMethod1()
         {
+            var kata = new Kata();
+            Assert.AreEqual("Black", kata.NotSoRandom(1, 1));
+            Assert.AreEqual("White", kata.NotSoRandom(2, 1));
+        }
+    }
+
+    public class Kata
+    {
+        public string NotSoRandom(int black, int white)
+        {
+            return "Black";
         }
     }
 }
