46bcf06c48c18dbec73ad13a19aa5239bd8f8c47
lulu.li29 <s987337613@gmail.com>
2018-01-28 20:24:10 +0800

    move simple fun and add test

---
+++ Simple-Fun/Simple-Fun/Kata.cs
@@ -0,0 +1,19 @@
+﻿namespace Simple_Fun
+{
+    public class Kata
+    {
+        public string NotSoRandom(double black, double white)
+        {
+            if (IsEven(black))
+            {
+                return "White";
+            }
+            return "Black";
+        }
+
+        private static bool IsEven(double black)
+        {
+            return black % 2 == 0;
+        }
+    }
+}
---
+++ Simple-Fun/Simple-Fun/KataTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+namespace Simple_Fun
+{
+    [TestFixture]
+    public class KataTest
+    {
+        [Test]
+        public void Test()
+        {
+            var kata = new Kata();
+            Assert.AreEqual("Black", kata.NotSoRandom(1, 1));
+            Assert.AreEqual("White", kata.NotSoRandom(2, 1));
+
+            Assert.AreEqual("Black", kata.NotSoRandom(1, 2));
+
+            Assert.AreEqual("White", kata.NotSoRandom(2, 2));
+
+            Assert.AreEqual("White", kata.NotSoRandom(6, 9));
+
+            Assert.AreEqual("White", kata.NotSoRandom(10000, 10000));
+
+            Assert.AreEqual("Black", kata.NotSoRandom(11111, 22222));
+        }
+    }
+}
